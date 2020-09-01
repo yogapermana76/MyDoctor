cc95fe23f6303030cd46c6e05cc2949dc62659a5
Yoga Permana <yoga@xcidic.com>
2020-09-01 19:11:04 +0700

    setup firebase

---
+++ src/config/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCEuzPh_E5JFmiZuZL-af1v8ft6FUsd3ao",
+  authDomain: "my-doctor-a2851.firebaseapp.com",
+  databaseURL: "https://my-doctor-a2851.firebaseio.com",
+  projectId: "my-doctor-a2851",
+  storageBucket: "my-doctor-a2851.appspot.com",
+  messagingSenderId: "241599265127",
+  appId: "1:241599265127:web:dfe4458cb6d448c5ef97c2"
+})
+
+const FireBase = firebase
+
+export default FireBase
---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+import FireBase from './firebase';
+
+export {FireBase}
