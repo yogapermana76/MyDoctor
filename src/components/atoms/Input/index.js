2fb8393c5457f7884089d996adc2042dfa114f29
Yoga Permana <yoga@xcidic.com>
2020-09-01 17:43:00 +0700

    add onFocus and onBlur in input text component

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, TextInput } from 'react-native'
 import { colors, fonts } from '../../../utils';
 
-const Input = (props) => {
+const Input = ({label}) => {
+  const [border, setBorder] = useState(colors.border)
+  const onFocusForm = () => {
+    setBorder(colors.tertiary)
+  }
+  const onBlurForm = () => {
+    setBorder(colors.border)
+  }
   return (
     <View>
-      <Text style={styles.label}>{props.label}</Text>
-      <TextInput style={styles.input} />
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        onFocus={onFocusForm}
+        onBlur={onBlurForm}
+        style={styles.input(border)}
+      />
     </View>
   )
 }
@@ -14,12 +25,12 @@ const Input = (props) => {
 export default Input;
 
 const styles = StyleSheet.create({
-  input: {
+  input: (border) => ({
     borderWidth: 1,
-    borderColor: colors.border ,
+    borderColor: border ,
     borderRadius: 10,
     padding: 12,
-  },
+  }),
   label: {
     fontSize: 16,
     color: colors.text.secondary,
